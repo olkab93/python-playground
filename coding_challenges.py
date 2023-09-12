c012e19fe0e1ed7f1fdd66f04de025d3d28e5b94
olka bocheńska <aleksandra.bochenska@netguru.com>
2023-09-12 13:08:55 +0200

    add remaining tasks and format a file

--- coding_challenges.py
+++ coding_challenges.py
@@ -1,4 +1,4 @@
-# 1. Converting Radians into Degrees
+# # 1. Converting Radians into Degrees
 # 1 rad to deg
 degree = 57.2957795
 
@@ -14,7 +14,6 @@ print(radian_to_deg(angle_to_convert))
 
 # --------------------------------------------------------------------------------------------
 # 2. Sort a List
-
 user_list = input("Pass me items to sort separated by a comma: ").split(",")
 sort = input("How should I sort the list? Type: asc, desc or none: ").lower()
 
@@ -45,7 +44,6 @@ print(sort_a_list(format_user_input(user_list), sort))
 
 # --------------------------------------------------------------------------------------------
 # 3. Convert Decimal Numbers to Binary
-
 def convert_decimal_to_binary(num_to_convert):
     reminders = []
     while num_to_convert >= 1:
@@ -59,9 +57,9 @@ def convert_decimal_to_binary(num_to_convert):
 
 print(convert_decimal_to_binary(125))
 
+
 # --------------------------------------------------------------------------------------------
 # 4. Count Vowels in a String
-
 def count_vowels_in_a_string(string_to_count):
     vowels = ["A", "E", "I", "O", "U"]
     vowels_count = 0
@@ -72,3 +70,49 @@ def count_vowels_in_a_string(string_to_count):
 
 
 print(count_vowels_in_a_string("COCONUT"))
+
+
+# --------------------------------------------------------------------------------------------
+# 5. Create a Calculator Function
+def calculator(num_1, operator, num_2):
+    if operator == "+":
+        return num_1 + num_2
+    elif operator == "-":
+        return num_1 - num_2
+    elif operator == "*":
+        return num_1 * num_2
+    elif operator == "/":
+        return num_1 / num_2
+    else:
+        return "I don't know this operator :( "
+
+
+print(calculator(2, "-", 8))
+
+
+# --------------------------------------------------------------------------------------------
+# 6. The Numbers
+items_list = [0, "coconut", "123", 123, "dog", 38973234, 28.4, "kangaroo"]
+
+
+def get_integers(items):
+    integers = []
+    for element in items_list:
+        if isinstance(element, int):
+            integers.append(element)
+    return integers
+
+
+print(get_integers(items_list))
+
+
+# --------------------------------------------------------------------------------------------
+# 7. Repeat Characters
+def double_letters(word):
+    word_doubled = ""
+    for letter in word:
+        word_doubled += letter * 2
+    return word_doubled
+
+
+print(double_letters("coconut"))
