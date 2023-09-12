d83b56b96e94fd9c2c129133d64bda13e2e25976
olka bocheńska <aleksandra.bochenska@netguru.com>
2023-09-12 12:24:21 +0200

    add tasks 1-3: converting radians to degrees, sort a list, convert decimal to binary

---
+++ coding_challenges.py
@@ -0,0 +1,60 @@
+# 1. Converting Radians into Degrees
+# 1 rad to deg
+degree = 57.2957795
+
+
+def radian_to_deg(angle_radian):
+    angle_degree = angle_radian * degree
+    return angle_degree
+
+
+angle_to_convert = int(input("Type angle value in radian: "))
+print(radian_to_deg(angle_to_convert))
+
+
+# --------------------------------------------------------------------------------------------
+# 2. Sort a List
+
+user_list = input("Pass me items to sort separated by a comma: ").split(",")
+sort = input("How should I sort the list? Type: asc, desc or none: ").lower()
+
+
+def format_user_input(user_input):
+    formatted_list = []
+    for item in user_input:
+        no_space = item.strip()
+        formatted_list.append(int(no_space))
+    return formatted_list
+
+
+def sort_a_list(list_to_sort, sort_type):
+    if sort_type == "asc":
+        list_to_sort.sort()
+        return list_to_sort
+    elif sort_type == "desc":
+        list_to_sort.sort(reverse=True)
+        return list_to_sort
+    elif sort_type == "none":
+        return list_to_sort
+    else:
+        print("I don't know how to sort your list.")
+
+
+print(sort_a_list(format_user_input(user_list), sort))
+
+
+# --------------------------------------------------------------------------------------------
+# 3. Convert Decimal Numbers to Binary
+
+def convert_decimal_to_binary(num_to_convert):
+    reminders = []
+    while num_to_convert >= 1:
+        reminders.append(str(int(num_to_convert % 2)))
+        num_to_convert = num_to_convert / 2
+
+    reminders.reverse()
+    binary = "".join(reminders)
+    return binary
+
+
+print(convert_decimal_to_binary(125))
